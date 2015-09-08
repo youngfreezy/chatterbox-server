146909f26d1ccdf693c4baad4ac7bac08c793a9f
youngfreezy <Fareez.Ahmed@gmail.com>
2015-09-08 15:25:43 -0700

    Added ability to perist data after server shut off

--- server/basic-server.js
+++ server/basic-server.js
@@ -13,14 +13,26 @@ app.use(cors());
 
 var responseObj = {};
 //responseObj['results'] = [];
-
+// looks up a file called "chatdata.txt"
 var data = fs.readFileSync(path.join(__dirname, './', 'chatdata.txt'));
-//1) Load the chat log if it exsits
+//1) Load the chat log if it exists
 if (data){
+  //we parse it, because it's stringified
   responseObj = JSON.parse(data);  
 }else{
+  // if there's no data, make an empty array.  
   responseObj['results'] = [];
 }
+
+var writeFile = function(content){
+  var ourcontent = content;
+    fs.writeFile(path.join(__dirname, './', 'chatdata.txt'), ourcontent, encoding='utf8', function (err, data) {
+      if (err) throw err;
+      console.log(data);
+    });
+  console.log("we wrote to the file")
+
+};
 
 //In current versions of node you MUST specify an ABSOLUTE location
   //relative location: ../myfolder/file.txt
@@ -33,7 +45,6 @@ if (data){
 
 // all environments
 app.get('/classes/messages', function(request, response) {
-
     response.send(JSON.stringify(responseObj));
 });
 app.get('/log', function(request, response) {
@@ -54,8 +65,12 @@ app.get('/*', function(request, response) {
 
 app.post('/classes/messages', bodyParser.json(), function(request, response) {
   // response.sendFile(path.join(__dirname, '../views', 'homepage.html'));
+  //everytime some posts, we want to write their data/message to the file. 
+  // we are overwriting the whole file.  
   console.log(typeof request.body);
+  
   responseObj.results.push(request.body);
+  writeFile(JSON.stringify(responseObj));
 });
 
 app.post('/classes/room1', bodyParser.json(), function(request, response) {
@@ -70,6 +85,7 @@ app.post('/*', bodyParser.json(), function(request, response){
 app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get("port"));
+
 
 //1) bodyparser.json()
 //2) OPTIONS
